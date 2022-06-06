379f4bbffe780eb2ef1cc2c851bc8d78d30ef075
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2022-06-06 14:24:18 +0200

    perfect power

--- katas/javaKatas/src/Abbreviator.java
+++ katas/javaKatas/src/Abbreviator.java
@@ -1,29 +1,14 @@
 import javax.swing.text.PlainDocument;
 
-/**
- * The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
-
-Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, following these rules:
-
-A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
-The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
- */
-
-
 
 public class Abbreviator {
     public static String abbreviate(String string) {
-        
         String resultado="";
         String palabra="";
-
         for (int i = 0; i < string.length(); i++) {
-            
-            if(string.charAt(i)==' ' || string.charAt(i)=='-' || i==string.length()-1){
-                System.out.println(palabra);
-                //System.out.println(string.charAt(i));
+            if(i==string.length()-1 || " -_'.;:,][1234567890".indexOf(string.charAt(i))!=-1){
                 if( i==string.length()-1){
-                    resultado+=abreviado(palabra);
+                    resultado+=abreviado(palabra+=string.charAt(string.length()-1));
                 }else{
                     resultado+=abreviado(palabra)+string.charAt(i);
                 }               
@@ -35,10 +20,6 @@ public class Abbreviator {
         return resultado;
       }
 
-
-      /**
-       * Método que abrevie la palabra 
-       */
       public static String abreviado(String palabra){
           if(palabra.length()>3){
         return  palabra.charAt(0)+Integer.toString(palabra.length()-2)+palabra.charAt(palabra.length()-1);
@@ -48,6 +29,6 @@ public class Abbreviator {
       }
 
       public static void main(String[] args) {
-          System.out.println(abbreviate("me quiero morir"));
+          System.out.println(abbreviate("sits-sits; mat, a"));
       }
 }
