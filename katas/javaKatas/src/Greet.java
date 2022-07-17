a88686e15d20499e30c4568c743f974946ca8895
lisb3th21 <epoaquiza@cifpfbmoll.eu>
2022-07-17 23:54:09 +0200

    jhnjh

--- katas/javaKatas/src/Greet.java
+++ katas/javaKatas/src/Greet.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Greet {
     public static String greet(String name)
   {
     return "Hello, " +name+" how are you doing today?";
+  }
+
+
+
+  public static void main(String[] args) {
+    Scanner input= new Scanner(System.in);
+
+    System.out.println("Cuál es tu nombre?");
+
+    String nombre = input.nextLine();
+
+    System.out.println(greet(nombre));
+
+
   }
 }
